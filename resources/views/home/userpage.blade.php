9a265af5724b07f7338c6f7bf36845772f7b8608
DASANAYAKALAGE OVINDU THANUJ PATHIRAJA <cb010931@students.apiit.lk>
2023-11-06 22:18:32 +0530

    First Commit

---
+++ resources/views/home/userpage.blade.php
@@ -0,0 +1,320 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Neat &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Oxygen:300,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="home/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="home/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="home/css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="home/css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="home/css/flexslider.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="home/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="home/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="home/js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	
+		{{-- //navigation bar is inside the header.blade.php --}}
+		@include('home.header')
+
+	<div class="container-wrap">
+		<aside id="fh5co-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/img_bg_1.jpg);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>Not Every Project Needs To Be Perfect</h1>
+										<h2>Free html5 templates Available at <a href="http://Themewagon.com/theme_tag/free/" target="_blank">Themewagon</a></h2>
+										<p><a class="btn btn-primary btn-demo" href="#"></i> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-push-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>WordPress Theme For People Who Tell Stories</h1>
+										<h2>Free html5 templates Available at <a href="http://Themewagon.com/theme_tag/free/" target="_blank">Themewagon</a></h2>
+										<p><a class="btn btn-primary btn-demo" href="#"></i> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container-fluids">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>What Would You Like To Learn?</h1>
+										<h2>Free html5 templates Available at <a href="http://Themewagon.com/theme_tag/free/" target="_blank">Themewagon</a></h2>
+										<p><a class="btn btn-primary btn-demo" href="#"></i> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_4.jpg);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-md-push-3 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>I Love to Tell My Story</h1>
+										<h2>Free html5 templates Available at <a href="http://Themewagon.com/theme_tag/free/" target="_blank">Themewagon</a></h2>
+										<p><a class="btn btn-primary btn-demo" href="#"></i> View Demo</a> <a class="btn btn-primary btn-learn">Learn More</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>	   	
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="fh5co-services">
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-diamond"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Brand Identity</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-lab2"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Web Design &amp; UI</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-settings"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Web Development</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-work" class="fh5co-light-grey">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Work</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<a href="work-single.html" class="work"  style="background-image: url(images/portfolio-1.jpg);">
+						<div class="desc">
+							<h3>Project Name</h3>
+							<span>Illustration</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a href="work-single.html" class="work" style="background-image: url(images/portfolio-2.jpg);">
+						<div class="desc">
+							<h3>Project Name</h3>
+							<span>Brading</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a href="work-single.html" class="work" style="background-image: url(images/portfolio-3.jpg);">
+						<div class="desc">
+							<h3>Project Name</h3>
+							<span>Illustration</span>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-counter" class="fh5co-counters">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center animate-box">
+					<p>We have a fun facts far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="row">
+						<div class="col-md-3 text-center">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="3452" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Cups of Coffee</span>
+						</div>
+						<div class="col-md-3 text-center">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="234" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Client</span>
+						</div>
+						<div class="col-md-3 text-center">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="6542" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Projects</span>
+						</div>
+						<div class="col-md-3 text-center">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="8687" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Done Projects</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-blog" class="blog-flex">
+			<div class="featured-blog" style="background-image: url(images/blog-1.jpg);">
+				<div class="desc-t">
+					<div class="desc-tc">
+						<span class="featured-head">Featured Posts</span>
+						<h3><a href="#">Top 20 Best WordPress Themes 2017 Multi Purpose and Creative Websites</a></h3>
+						<span><a href="#" class="read-button">Learn More</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="blog-entry fh5co-light-grey">
+				<div class="row animate-box">
+					<div class="col-md-12">
+						<h2>Latest Posts</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<a href="#" class="blog-post">
+							<span class="img" style="background-image: url(images/blog-2.jpg);"></span>
+							<div class="desc">
+								<h3>26 Best Education WordPress Themes 2017 You Need To See</h3>
+								<span class="cat">Collection</span>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 animate-box">
+						<a href="#" class="blog-post">
+							<span class="img" style="background-image: url(images/blog-1.jpg);"></span>
+							<div class="desc">
+								<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
+								<span class="cat">Collection</span>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 animate-box">
+						<a href="#" class="blog-post">
+							<span class="img" style="background-image: url(images/blog-3.jpg);"></span>
+							<div class="desc">
+								<h3>16 Outstanding Photography WordPress Themes You Must See</h3>
+								<span class="cat">Collection</span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- END container-wrap -->
+
+	<div class="container-wrap">
+		
+		@include('home.footer')
+
+
+	</div><!-- END container-wrap -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="home/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="home/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="home/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="home/js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="home/js/jquery.flexslider-min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="home/js/jquery.magnific-popup.min.js"></script>
+	<script src="home/js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="home/js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="home/js/main.js"></script>
+
+	</body>
+</html>
+
